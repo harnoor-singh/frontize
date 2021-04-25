15d056b7f2130536355f16eafb72869a663f282c
AvKat <kathpal.adil@gmail.com>
2021-04-26 01:09:24 +0530

    Minor change to solve author profile bug

--- packages/blog/src/pages/api/authors/[slug].ts
+++ packages/blog/src/pages/api/authors/[slug].ts
@@ -2,13 +2,17 @@ import fs from "fs";
 import { join } from "path";
 import matter from "gray-matter";
 import { NextApiRequest, NextApiResponse } from "next";
+import getConfig from "next/config";
 
-const authorsDirectory = join(process.cwd(), "_authors");
+const { serverRuntimeConfig } = getConfig();
+const projectRoot = serverRuntimeConfig.PROJECT_ROOT;
+
+const authorsDirectory = join(projectRoot, "_authors");
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
   const { slug } = req.query;
-
   const fullPath = join(authorsDirectory, `${slug}.md`);
+
   const fileContents = fs.readFileSync(fullPath, "utf8");
   const { data } = matter(fileContents);
 
