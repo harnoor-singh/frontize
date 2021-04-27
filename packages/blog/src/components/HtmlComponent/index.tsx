f651ffa4f15f750d0c018efe7ff3ff37aa5a0d38
AvKat <kathpal.adil@gmail.com>
2021-04-27 23:29:36 +0530

    style: add style to posts
    Since base tailwind disables all typography styles of tags, this was a way to add styles to the post content.

--- packages/blog/src/components/HtmlComponent/index.tsx
+++ packages/blog/src/components/HtmlComponent/index.tsx
@@ -1,5 +1,7 @@
 import React, { useEffect } from "react";
 import { useRemark } from "react-remark";
+import rehypeSlug from "rehype-slug";
+import styles from "./html.module.css";
 
 export interface HtmlComponentProps {
   content: string;
@@ -12,7 +14,20 @@ const HtmlComponent: React.FC<HtmlComponentProps> = ({
   className,
   lineBreaks = true,
 }) => {
-  const [mdContent, setMDContent] = useRemark();
+  const [mdContent, setMDContent] = useRemark({
+    rehypePlugins: [rehypeSlug],
+    rehypeReactOptions: {
+      components: {
+        h1: (props: any) => (
+          <a href={`#${props.id}`}>
+            <h1 className={styles.heading} {...props} />
+          </a>
+        ),
+        p: (props: any) => <p className={styles.paragraph} {...props} />,
+        a: (props: any) => <a className={styles.link} {...props} />,
+      },
+    },
+  });
 
   useEffect(() => {
     setMDContent(content);
