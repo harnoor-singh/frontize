0adbc4eb635dddfe1cbc64eb94c60a44ed04f885
AvKat <kathpal.adil@gmail.com>
2021-05-05 20:11:59 +0530

    chore: minor changes

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -3,6 +3,10 @@ import express from "express";
 const PORT = 3006;
 const app = express();
 
+app.get("*", (req, res) => {
+  res.send("Syccess");
+});
+
 app.listen(PORT, () => {
   console.log(`App started on port ${PORT}`);
 });
