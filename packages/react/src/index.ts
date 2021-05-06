1591c87c4ebd2f0e91e69208e5ca12f84c953311
AvKat <kathpal.adil@gmail.com>
2021-05-06 12:52:25 +0530

    chore: unified package versions

--- packages/react/src/index.ts
+++ packages/react/src/index.ts
@@ -1,10 +1,13 @@
 import express from "express";
+import { join } from "path";
 
 const PORT = 3006;
 const app = express();
 
+app.use(express.static(join(__dirname, "..", "ui", "build")));
+
 app.get("*", (req, res) => {
-  res.send("Syccess");
+  res.send(join(__dirname, "..", "ui", "build", "index.html"));
 });
 
 app.listen(PORT, () => {
